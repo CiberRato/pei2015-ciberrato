ccaa146bc2dd4a1dd45223517a9faab74fcdf18f
Bruno Silva <brunosilva@aluno-2038.wireless.ua.pt>
2015-02-16 09:55:02 +0000

    adicionado a ligacao tcp ao django

--- src/server/viewer.py
+++ src/server/viewer.py
@@ -1,12 +1,19 @@
 import socket
 
 def main():
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 	sock.sendto("<View/>\n" ,("127.0.0.1", 6000))
 
+    send_socket.connect(("127.0.0.1", TCP_PORT))
 	while 1:
 		data, (host, port) = sock.recvfrom(1024)
 		print data
+        send_socket.send(data)
+
+    send_socket.close()
+    sock.close()
 
 if __name__ == "__main__":
 	main()
