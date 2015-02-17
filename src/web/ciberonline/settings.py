8bc00fe489fc551cbea08ab3bb69ab982172b2b5
Rodrigo Cunha <RodrigoCunha@MBP-de-Rodrigo.lan>
2015-02-17 20:10:35 +0000

    Authentication app created - Django

--- src/web/ciberonline/settings.py
+++ src/web/ciberonline/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
+    'groups',
 )
 
 MIDDLEWARE_CLASSES = (
