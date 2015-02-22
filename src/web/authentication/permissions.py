9cccc8dd9ce8890ef09bb5fa9476d02642baa350
Rodrigo Cunha <RodrigoCunha@MBP-de-Rodrigo.lan>
2015-02-22 13:14:58 +0000

    Added comments

--- src/web/authentication/permissions.py
+++ src/web/authentication/permissions.py
@@ -1,6 +1,9 @@
 from rest_framework import permissions
 
 class IsAccountOwner(permissions.BasePermission):
+	"""
+	Verify if user is the Account Owner
+	"""
 	def has_object_permission(self, request, view, obj):
 		if request.user:
 			return obj == request.user
