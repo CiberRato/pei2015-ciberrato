098799566404238f5d39df1a7a3a2b3c6fe75bdb
Rafael Ferreira <mail@rafaelferreira.pt>
2015-02-25 19:29:37 +0000

    models.ForeignKey Group

--- src/web/competition/models.py
+++ src/web/competition/models.py
@@ -70,6 +70,7 @@ class CompetitionAgent(models.Model):
     competition = models.ForeignKey(Competition, blank=False)
     round = models.ForeignKey(Round, blank=False)
     agent = models.ForeignKey(Agent, blank=False)
+    group = models.ForeignKey(Group, blank=False)
 
     eligible = models.BooleanField(default=True)
 
