1dcc795eee84157bf85886b1644897098e1eaef7
Rafael Ferreira <mail@rafaelferreira.pt>
2015-02-25 09:50:41 +0000

    removed the nested because it's unnecessary for our project

--- src/web/ciberonline/urls.py
+++ src/web/ciberonline/urls.py
@@ -7,26 +7,23 @@ from authentication.views import AccountViewSet, LoginView, LogoutView
 from groups.views import GroupMembersViewSet, AccountGroupsViewSet, GroupViewSet, MakeMemberAdminViewSet, MemberInGroupViewSet
 from competition.views import GetSimulation
 
-from rest_framework_nested import routers
+from rest_framework import routers
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
 
+# GROUPS URSL
 router.register(r'group_members', GroupMembersViewSet)
 router.register(r'user_groups', AccountGroupsViewSet)
 router.register(r'group', GroupViewSet)
 router.register(r'make_group_admin', MakeMemberAdminViewSet)
 router.register(r'group_member', MemberInGroupViewSet)
+# GROUPS URSL
 
 router.register(r'get_simulation', GetSimulation, 'Get simulation')
 
-account_router = routers.NestedSimpleRouter(
-    router, r'accounts', lookup='account'
-)
 urlpatterns = patterns('',
-    #url(r'^admin/', include(admin.site.urls)),
     url(r'^api/v1/', include(router.urls)),
-    url(r'^api/v1/', include(account_router.urls)),
     url(r"api/v1/auth/login/$", LoginView.as_view(), name="login"),
     url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
     url(r'^api-auth/', include('rest_framework.urls',
