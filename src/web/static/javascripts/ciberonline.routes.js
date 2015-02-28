7c41c4a16c66c6f809e7d81abf23dbbe3216e8ae
Rodrigo Cunha <RodrigoCunha@MBP-de-Rodrigo.lan>
2015-02-28 15:33:32 +0000

    authentication angular

--- src/web/static/javascripts/ciberonline.routes.js
+++ src/web/static/javascripts/ciberonline.routes.js
@@ -8,7 +8,7 @@
     config.$inject = ['$routeProvider'];
 
     function config($routeProvider){
-        $routeProvider.when('/register',{
+        $routeProvider.when('/idp/register',{
             controller: 'RegisterController',
             controllerAs: 'vm',
             templateUrl: '/static/templates/authentication/register.html'
