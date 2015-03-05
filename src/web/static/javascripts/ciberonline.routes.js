5c0c4794ba32d7fd2a3c068d522308f6ac01f776
Rafael Ferreira <mail@rafaelferreira.pt>
2015-03-05 22:38:53 +0000

    change routes for chrome

--- src/web/static/javascripts/ciberonline.routes.js
+++ src/web/static/javascripts/ciberonline.routes.js
@@ -8,33 +8,33 @@
     config.$inject = ['$routeProvider'];
 
     function config($routeProvider){
-        $routeProvider.when('/idp/register/',{
+        $routeProvider.when('/idp/register',{
             controller: 'RegisterController',
             controllerAs: 'vm',
             templateUrl: '/static/templates/authentication/register.html'
-        }).when('/idp/login/',{
+        }).when('/idp/login',{
             controller: 'LoginController',
             controllerAs: 'vm',
             templateUrl: 'static/templates/authentication/login.html'
         }).when('/panel/', {
             templateUrl: '/static/templates/panel/index.html'
-        }).when('/panel/:username/changePassword/',{
+        }).when('/panel/:username/changePassword',{
             controller: 'ProfileController',
             controllerAs: 'vm',
             templateUrl: '/static/templates/panel/changePassword.html'
-        }).when('/panel/:username/editProfile/',{
+        }).when('/panel/:username/editProfile',{
             controller: 'ProfileController',
             controllerAs: 'vm',
             templateUrl: '/static/templates/panel/editProfile.html'
-        }).when('/panel/:username/myTeams/',{
+        }).when('/panel/:username/myTeams',{
             controller: 'ProfileController',
             controllerAs: 'vm',
             templateUrl: '/static/templates/panel/myTeams.html'
-        }).when('/panel/:username/createTeam/',{
+        }).when('/panel/:username/createTeam',{
             controller: 'CreateTeamController',
             controllerAs: 'vm',
             templateUrl: '/static/templates/panel/createTeam.html'
-        }).when('/panel/allTeams/',{
+        }).when('/panel/allTeams',{
             templateUrl: '/static/templates/panel/allTeams.html'
         }).when('/',{
             templateUrl: '/templates/index.html'
