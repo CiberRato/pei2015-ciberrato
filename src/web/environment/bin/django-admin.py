e268f6b00ef36fa79a782371494e883cadbeee5e
Rodrigo Cunha <RodrigoCunha@MacBook-Pro-de-Rodrigo.local>
2015-03-08 21:16:37 +0000

    jquery

---
+++ src/web/environment/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/RodrigoCunha/Desktop/StudyWorkTeam/3Ano/2Semestre/PEI/src/web/environment/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
