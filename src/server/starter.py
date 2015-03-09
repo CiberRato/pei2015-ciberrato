5eaa2e59450721fdd4c4d599ae8092a671d46652
Bruno Silva <brunomiguelsilva@ua.pt>
2015-03-09 20:45:43 +0000

    -changed end point handling -added -nogui option to the simulator -added json converting (still in progress)

--- src/server/starter.py
+++ src/server/starter.py
@@ -8,31 +8,35 @@ import json
 import os
 
 def main():
-
 	viewer_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	viewer_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-	HOST = "http://127.0.0.1:8000/"
-	result = requests.get(HOST + "api/v1/get_simulation/")
+	HOST = "http://127.0.0.1:8000"
+	result = requests.get(HOST + "/api/v1/competitions/get_simulations/")
 	simJson = json.loads(result.text)[0]
 	tempFilesList = {}
 	for key in simJson:
+		if key == "agents" or key == "simulation_id":
+			continue
+			#Handle agents and simulation id
 		fp = tempfile.NamedTemporaryFile()
-		fp.write(requests.get(HOST + simJson[key]).text)
+		r = requests.get(HOST + simJson[key])
+		fp.write(r.text)
 		fp.seek(0)
 		tempFilesList[key] = fp
+
+
 
 	print "Process ID: ", os.getpid()
 	print "Creating process for simulator.."
 	##		CHECK ./simulator --help 				##
 	# Run simulator for LINUX
 	simulator = subprocess.Popen(["./cibertools-v2.2/simulator/simulator", \
-	 				"-param", 	tempFilesList["param_list_path"].name, \
-	 				"-lab", 	tempFilesList["lab_path"].name, \
-	 				"-grid", 	tempFilesList["grid_path"].name], \
+	                "-nogui", \
+	 				"-param", 	tempFilesList["param_list"].name, \
+	 				"-lab", 	tempFilesList["lab"].name, \
+	 				"-grid", 	tempFilesList["grid"].name], \
 	 				stdout = subprocess.PIPE)
-
-	print tempFilesList["param_list_path"].name
 
 	print "Successfully opened process with process id: ", simulator.pid
 	time.sleep(1)
@@ -47,24 +51,27 @@ def main():
 
 	print "Viewer ready.."
 
-	# print "Creating docker for agent.."
-	# docker = subprocess.Popen(["docker", "run", "-H", "tcp://192.168.59.103:2376",\
-	# 							"python", "robsample_python.py",\
-	# 							"--host", "172.17.42.1",\
-	# 							"--pos", "1"], stdout=subprocess.PIPE)
-	# docker_container = docker.stdout.readline().strip()
-	# docker.wait()
-
-	# print "Successfully opened container: ", docker_container
-	# time.sleep(1)
-
 	n_agents = 5
 	print "Sending message telling viewer how many agents there are..."
 	viewer_c.send('<Robots Amount="'+str(n_agents)+'" />')
 	for i in range(1, n_agents+1, 1):
 		print "Opening Agent - " + str(i)
-		agent = subprocess.Popen(["python", "./cibertools-v2.2/robsample/robsample_python.py", "--pos", str(i)], stdout=subprocess.PIPE)
+		agent = subprocess.Popen(["python", "./cibertools-v2.2/robsample/robsample_python.py", "-pos", str(i)], stdout=subprocess.PIPE)
+
+		# print "Creating docker for agent.."
+		# docker = subprocess.Popen(["docker", "run", "-d", "-P","ubuntu/ciberonline",\
+		# 						"python", "./cibertools-v2.2/robsample/robsample_python.py",\
+		# 						"--pos", str(i), "--host", "172.17.42.1"], stdout=subprocess.PIPE)
+		# docker_container = docker.stdout.readline().strip()
+		# docker.wait()
+		# print "Successfully opened container: ", docker_container
+
 		print "Successfully opened agent " + str(i) + " with process id: ", agent.pid
+
+	data = viewer_c.recv(4096)
+	while data != "<AllRobotsRegistered/>":
+		data = viewer_c.recv(4096)
+		print data
 
 	print "Sending message to Viewer (everything is ready to start)"
 	viewer_c.send("<StartedAgents/>")
--- src/server/viewer.py
+++ src/server/viewer.py
@@ -3,10 +3,11 @@ import socket
 import time
 from xml.dom import minidom
 from collections import OrderedDict
+import json
 
 def main():
 	log_name = "ciberOnline_log"
-	log = open("log", "w") # log file used to view prints of this program
+	log = open("log.txt", "w") # log file used to view prints of this program
 	log.write("viewer started")
 
 	simulator_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -18,7 +19,7 @@ def main():
 	parametersXML = minidom.parseString(data.replace("\x00", ""))
 	itemlist = parametersXML.getElementsByTagName('Parameters')
 	simTime = itemlist[0].attributes['SimTime'].value
-	print "SimTime: ", simTime
+	log.write("SimTime: "+ simTime+"\n")
 
 	log_file = open(log_name, "w")
 	log_file.write(data)
@@ -40,17 +41,20 @@ def main():
 	checkedRobots = []
 	while len(checkedRobots) != int(robotsAmount):
 		data, (host, port) = simulator_s.recvfrom(1024)
+		#starter_s.send(data)
 		robotsXML = minidom.parseString(data.replace("\x00", ""))
 		robots = robotsXML.getElementsByTagName('Robot')
 		if len(robots):
 			robotID = robots[0].attributes['Id'].value
-			log.write(robotID)
+			log.write(robotID + "\n	")
 			checkedRobots += [robotID]
 			checkedRobots = list(OrderedDict.fromkeys(checkedRobots))
 			log.write(str(checkedRobots) + "\n	")
-			log.write(str(len(checkedRobots)) + "\n")
+			#log.write(str(len(checkedRobots)) + "\n")
+
 
 	log.write("All Robots are registered\n")
+	starter_s.send("<AllRobotsRegistered/>")
 
 	data = starter_s.recv(4096)
 	while data != "<StartedAgents/>":
@@ -62,6 +66,7 @@ def main():
 	#django_tcp.bind(("127.0.0.1", 7000))
 	#django_tcp.listen(1)
 	#django_s, django_addr = django_tcp.accept()
+	jsonFile = open("test.json", "w")
 	while simTime != robotTime:
 		data = simulator_s.recv(4096)
 		# Actualizar o tempo do robot
@@ -69,9 +74,13 @@ def main():
 		robotXML = minidom.parseString(data)
 		itemlist = robotXML.getElementsByTagName('Robot')
 		robotTime = itemlist[0].attributes['Time'].value
+
+		#Convert to json
+		robot_json = json.dumps(robotXML, skipkeys=True)
+
 		# Enviar os dados da simulação para o exterior
 		#django_s.send(data)
-		log_file.write(data)
+		log_file.write(robot_json)
 
 	starter_s.send("<EndedSimulation/>")
 
