78a9a992497a89b344684a0cb199133dcc1a111f
Diogo Silva <dbtds@ua.pt>
2015-03-15 11:03:01 +0000

    Removed env option and forced user to run it as root

--- install.sh
+++ install.sh
@@ -1,54 +1,38 @@
-if [ "$1" = "--help" ]; then
-	echo "Please use this script under a Debian distribution"
-	echo ""
-	echo "Usage: ./install.sh [argument]"
-	echo "Arguments:"
-	echo "	--help - Shows this message"
-	echo "	--env  - Use virtual environment for web"
-	exit
-fi
-
-if [ "$1" = "--env" ]; then
-	ENV=true
+if [ "$(id -u)" != "0" ]; then
+	echo "This script must be run as root" 1>&2
+	exit 1
 fi
 
 echo "	>> Installing general dependencies"
-sudo apt-get install -y	python \
+apt-get install -y	python \
 			build-essential \
 			g++ \
 			qt4-dev-tools \
 			python-pip \
 			python-virtualenv \
 			docker.io
-(cd src/web; 
-if [ "$ENV" ]; 
-then
-	echo "	>> Configuring virtual environment"
-	if [ ! -d "environment" ]; then
-		virtualenv environment
-	fi
-	source environment/bin/activate;
-	echo "	>> Installing python dependencies"
-	pip install -r requirements.txt;
-else
-	echo "	>> Installing python dependencies"
-	sudo pip install -r requirements.txt;
-fi
+(cd src/web;
+echo "	>> Installing python dependencies"
+pip install -r requirements.txt;
+
 echo "	>> Migrating Django applications"
-python manage.py migrate;
-if [ "$ENV" ]; then
-	deactivate
-fi)
+python manage.py migrate;)
+
 echo "	>> Compiling cibertools"
 (cd src/server/cibertools-v2.2/;
 make;)
 echo "	>> Creating docker image based on Dockerfile"
-sudo groupadd docker
-sudo gpasswd -a $USER docker
-if [ $(lsb_release -a | grep "Ubuntu 14.04" | wc -l) != "0" ]; then
-	sudo alias docker="docker.io"
+groupadd docker
+gpasswd -a $USER docker
+
+if [ $(lsb_release -a | grep "Ubuntu 14.04" | wc -l) != "0" ];
+then
+	service docker.io restart
+	(cd src/server/;
+	docker.io build -t ubuntu/ciberonline .)
+else
+	service docker restart
+	(cd src/server/;
+	docker build -t ubuntu/ciberonline .)
 fi
-sudo service docker restart
-(cd src/server/;
-sudo docker build -t ubuntu/ciberonline .;)
 echo "	Please logout and login again"
