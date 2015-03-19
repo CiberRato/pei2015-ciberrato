87bdf766d84cf279acf61c238513c6d519ddb1fd
Bruno Silva <brunomiguelsilva@ua.pt>
2015-03-19 18:10:56 +0000

    GetSimId now loads settings from json file

--- src/server/GetSimId.py
+++ src/server/GetSimId.py
@@ -1,14 +1,18 @@
 import cherrypy
-import subprocess
 import socket
-
+import re
 
 class GetSimId(object):
 
 	def __init__(self):
+		settings_str = re.sub("///.*", "", open("settings.json", "r").read())
+		settings = json.loads(settings_str)
+		HOST = settings["settings"]["starter_end_point_host"]
+		PORT = settings["settings"]["starter_end_point_port"]
+
 		self.starter_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.starter_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		self.starter_tcp.connect(("127.0.0.1", 7500))
+		self.starter_tcp.connect((HOST, PORT))
 
 	@cherrypy.expose
 	def index(self, **kwargs):
@@ -23,6 +27,12 @@ class GetSimId(object):
 
 
 if __name__ == '__main__':
-	cherrypy.config.update({'server.socket_host': '127.0.0.1',\
-							'server.socket_port': 9000 })
+	settings_str = re.sub("///.*", "", open("settings.json", "r").read())
+	settings = json.loads(settings_str)
+	HOST = settings["settings"]["end_point_host"]
+	PORT = settings["settings"]["end_point_port"]
+
+
+	cherrypy.config.update({'server.socket_host': HOST ,\
+							'server.socket_port': PORT })
 	cherrypy.quickstart(GetSimId(), "/api/v1/simulation_id/")
