22ca08b34bb1012ee8e43b4de6144f8e7bc88873
Bruno Silva <brunomiguelsilva@ua.pt>
2015-03-19 21:29:04 +0000

    small bug fixes

--- src/server/test_endpoint.py
+++ src/server/test_endpoint.py
@@ -4,7 +4,7 @@ import re
 def main():
 	settings_str = re.sub("///.*", "", open("settings.json", "r").read())
 	settings = json.loads(settings_str)
-	HOST = settings["url"]["post_sim_id"]
+	HOST = settings["urls"]["post_sim_id"]
 
 	params = {'simulation_identifier': "0a256950-7a5c-403d-aba3-52e455d197c5" }
 	result = requests.post(HOST, params)
