0b988ffacb95901fe822a169030f2dfaf466172b
Bruno Silva <brunomiguelsilva@ua.pt>
2015-03-19 18:04:14 +0000

    viewer now reads all settings from the json file

--- src/server/viewer.py
+++ src/server/viewer.py
@@ -14,14 +14,17 @@ def main():
 	settings_str = re.sub("///.*", "", open("settings.json", "r").read())
 	settings = json.loads(settings_str)
 
-	VIEWER_HOST = settings["settings"]["starter_end_point_host"]
-	VIEWER_PORT = settings["settings"]["starter_end_point_port"]
-
 	SIMULATOR_HOST = settings["settings"]["simulator_host"]
 	SIMULATOR_PORT = settings["settings"]["simulator_port"]
 
 	STARTER_HOST = settings["settings"]["starter_viewer_host"]
 	STARTER_PORT = settings["settings"]["starter_viewer_port"]
+
+	WEBSOCKET_HOST = settings["settings"]["websocket_host"]
+	WEBSOCKET_PORT = settings["settings"]["websocket_port"]
+
+	FILE = settings["settings"]["tmp_file"]
+	#end of loading settings
 
 	if self.wlog:
 		log = open("log", "w") # log file used to view prints of this program
@@ -30,7 +33,7 @@ def main():
 	simulator_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	simulator_s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-	simulator_s.sendto("<View/>\n" ,("127.0.0.1", 6000))
+	simulator_s.sendto("<View/>\n" ,(SIMULATOR_HOST, SIMULATOR_PORT))
 	# Ler o valor do tempo de simulação e obter as portas
 	data, (host, port) = simulator_s.recvfrom(4096)
 	# Vem params, grid e lab aqui
@@ -39,7 +42,8 @@ def main():
 	simTime = itemlist[0].attributes['SimTime'].value
 
 	# Write params, grid and lab to the json Log
-	log_file = open(log_name, "w")
+	log_file = open(FILE, "w")
+
 	data = data.split("<Lab")
 	data[1] = "<Lab" + data[1]
 	data[1] = data[1].split("<Grid")
@@ -69,7 +73,7 @@ def main():
 
 	starter_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	starter_s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	starter_s.connect(("127.0.0.1", 7000))
+	starter_s.connect((STARTER_HOST, STARTER_PORT))
 
 	# Viewer continua a ouvir enquanto o Starter não lhe mandar começar a simulação
 	data = starter_s.recv(4096)
@@ -77,9 +81,9 @@ def main():
 	robots = robotsXML.getElementsByTagName('Robots')
 	robotsAmount = robots[0].attributes['Amount'].value
 
-	log.write("Robots Amount: " + robotsAmount + "\n")
-
-	log.write("checking Robots\n")
+	if self.wlog:
+		log.write("Robots Amount: " + robotsAmount + "\n")
+		log.write("checking Robots\n")
 
 	checkedRobots = []
 	while len(checkedRobots) != int(robotsAmount):
@@ -89,20 +93,24 @@ def main():
 		if len(robots):
 			for r in robots:
 				robotID = r.attributes['Id'].value
-				log.write(robotID + "\n	")
+				if self.wlog:
+					log.write(robotID + "\n	")
 				checkedRobots += [robotID]
 				checkedRobots = list(OrderedDict.fromkeys(checkedRobots))
-				log.write(str(checkedRobots) + "\n	")
-				log.write(str(len(checkedRobots)) + "\n")
+				if self.wlog:
+					log.write(str(checkedRobots) + "\n	")
+					log.write(str(len(checkedRobots)) + "\n")
 
-
-	log.write("All Robots are registered\n")
+	if self.wlog:
+		log.write("All Robots are registered\n")
 	starter_s.send("<AllRobotsRegistered/>")
 
 	data = starter_s.recv(4096)
 	while data != "<StartedAgents/>":
 		data = starter_s.recv(4096)
-	log.write("Received start confirmation")
+
+	if self.wlog:
+		log.write("Received start confirmation\n")
 
 	simulator_s.sendto("<Start/>\n" ,(host, port))
 
@@ -110,14 +118,15 @@ def main():
 
 	websocket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	websocket_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	websocket_tcp.connect(("127.0.0.1", 10000))
+	websocket_tcp.connect((WEBSOCKET_HOST, WEBSOCKET_PORT))
 
 	robotTime = 0
 	while simTime != robotTime:
 		data = simulator_s.recv(4096)
 		# Actualizar o tempo do robot
 		data = data.replace("\x00", "")
-		#log.write(data)
+		if self.wlog:
+			log.write(data + "\n")
 		robotXML = minidom.parseString(data)
 		itemlist = robotXML.getElementsByTagName('LogInfo')
 		robotTime = itemlist[0].attributes['Time'].value
@@ -141,8 +150,9 @@ def main():
 
 	starter_s.send('<EndedSimulation LogFile="' + log_name + '" />')
 
+	if self.wlog:
+		log.close()
 	websocket_tcp.close()
-	log.close()
 	log_file.close()
 	starter_s.close()
 	simulator_s.close()
