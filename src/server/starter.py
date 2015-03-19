e53a826a57913d70935ccc8ecf4e0a4ea8b8a22a
Bruno Silva <brunomiguelsilva@ua.pt>
2015-03-19 21:21:51 +0000

    added some error messages for some situations

--- src/server/starter.py
+++ src/server/starter.py
@@ -9,15 +9,22 @@ import json
 import os
 import sys
 import tarfile
+import re
 from xml.dom import minidom
 
 
 def main():
-	print "Starter is in deamon mode, waiting for simulation.."
+	#loading settings
+	settings_str = re.sub("///.*", "", open("settings.json", "r").read())
+	settings = json.loads(settings_str)
 
+	END_POINT_HOST = settings["settings"]["starter_end_point_host"]
+	END_POINT_PORT = settings["settings"]["starter_end_point_port"]
+
+	print "Starter is in deamon mode, waiting for simulation.."
 	end_point_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	end_point_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	end_point_tcp.bind(("127.0.0.1", 7500))
+	end_point_tcp.bind((END_POINT_HOST, END_POINT_PORT))
 	end_point_tcp.listen(1)
 	end_point_c, end_point_add = end_point_tcp.accept()
 
@@ -27,26 +34,36 @@ def main():
 			data = end_point_c.recv(1024)
 		print "Received simulation with sim_id= " + data + ", starting now.."
 		run(data)
+		print "Simulation " + data + " finished successfully.."
 		data = None
 
 def run(sim_id):
-	# DOCKERIP = None
-	# for interface in netifaces.interfaces():
-	# 	if interface.startswith('docker'):
-	# 		DOCKERIP = netifaces.ifaddresses(interface)[2][0]['addr']
-	# 		break
-	# if DOCKERIP == None:
-	# 	print "Please check your docker interface."
-	# 	exit(-1)
-	# else:
-	# 	print "Docker interface: %s" % (DOCKERIP, )
+	DOCKERIP = None
+	for interface in netifaces.interfaces():
+		if interface.startswith('docker'):
+			DOCKERIP = netifaces.ifaddresses(interface)[2][0]['addr']
+			break
+	if DOCKERIP == None:
+		print "Please check your docker interface."
+		exit(-1)
+	else:
+		print "Docker interface: %s" % (DOCKERIP, )
 
-	viewer_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	viewer_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	#loading settings
+	GET_SIM_HOST = settings["urls"]["get_simulation"]
 
-	HOST = "http://127.0.0.1:8000"
+	POST_SIM_HOST = settings["urls"]["get_simulation"]
 
-	result = requests.get(HOST + "/api/v1/competitions/get_simulation/" + sim_id + "/")
+	VIEWER_HOST = settings["settings"]["starter_viewer_host"]
+	VIEWER_PORT = settings["settings"]["starter_viewer_port"]
+
+	JSON_FILE = settings["settings"]["ciberOnline_log.json"]
+
+	TAR_FILE = settings["settings"]["log_<SIM_ID>.tar.gz"]
+	#end loading settings
+
+	result = requests.get(HOST + sim_id + "/")
+	print result.text
 	simJson = json.loads(result.text)
 	tempFilesList = {}
 	n_agents = 0
@@ -58,7 +75,9 @@ def run(sim_id):
 			agents = simJson[key]
 			continue
 		if key == "simulation_id":
-			sim_id = simJson[key]
+			if sim_id != simJson[key]:
+				print "ERROR: sim_id received not the the same in the simulation"
+				return
 			continue
 
 		fp = tempfile.NamedTemporaryFile()
@@ -68,6 +87,7 @@ def run(sim_id):
 		tempFilesList[key] = fp
 
 	print "Number of agents to be loaded: " + str(n_agents)
+
 	print "Process ID: ", os.getpid()
 	print "Creating process for simulator"
 	##		CHECK ./simulator --help 				##
@@ -87,7 +107,10 @@ def run(sim_id):
 	viewer = subprocess.Popen(["python", "viewer.py"], stdout=subprocess.PIPE)
 	print "Successfully opened process with process id: ", viewer.pid
 
-	viewer_tcp.bind(("127.0.0.1", 7000))
+	viewer_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	viewer_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+	viewer_tcp.bind((VIEWER_HOST, VIEWER_PORT))
 	viewer_tcp.listen(1)
 	viewer_c, viewer_c_addr = viewer_tcp.accept()
 
@@ -95,19 +118,19 @@ def run(sim_id):
 	viewer_c.send('<Robots Amount="' +str(n_agents)+'" />')
 
 	for i in range(n_agents):
-		agent = subprocess.Popen(["python", "./cibertools-v2.2/robsample/robsample_python.py", "-pos", str(i)], stdout=subprocess.PIPE)
-		# print "Creating docker for agent: \n\tName: %s\n\tPosition: %s\n\tLanguage: %s" % \
-		# 		(agents[i]['agent_name'], agents[i]['pos'], agents[i]['language'], )
-		# docker = subprocess.Popen("docker run -d ubuntu/ciberonline " \
-		# 						  "bash -c 'curl " \
-		# 						  "http://%s:8000%s" \
-		# 						  " | tar -xz;" \
-		# 						  " python myrob.py -host %s -pos %s'" %  \
-		# 						  (DOCKERIP, agents[i]['files'], DOCKERIP, agents[i]['pos'], ),
-		# 						  shell = True, stdout = subprocess.PIPE)
-		# docker_container = docker.stdout.readline().strip()
-		# docker.wait()
-		# print "Successfully opened container: %s\n" % (docker_container, )
+		#agent = subprocess.Popen(["python", "./cibertools-v2.2/robsample/robsample_python.py", "-pos", str(i)], stdout=subprocess.PIPE)
+		print "Creating docker for agent: \n\tName: %s\n\tPosition: %s\n\tLanguage: %s" % \
+				(agents[i]['agent_name'], agents[i]['pos'], agents[i]['language'], )
+		docker = subprocess.Popen("docker run -d ubuntu/ciberonline " \
+								  "bash -c 'curl " \
+								  "http://%s:8000%s" \
+								  " | tar -xz;" \
+								  " python myrob.py -host %s -pos %s'" %  \
+								  (DOCKERIP, agents[i]['files'], DOCKERIP, agents[i]['pos'], ),
+								  shell = True, stdout = subprocess.PIPE)
+		docker_container = docker.stdout.readline().strip()
+		docker.wait()
+		print "Successfully opened container: %s\n" % (docker_container, )
 
 	data = viewer_c.recv(4096)
 	while data != "<AllRobotsRegistered/>":
@@ -137,31 +160,31 @@ def run(sim_id):
 	simulator.terminate()
 	simulator.wait()
 
-	#read json log and turn it in a str
-	logXML = minidom.parseString(data)
-	log_name = logXML.getElementsByTagName('EndedSimulation')
-	file_name = log_name[0].attributes['LogFile'].value
-
-	json_gz = tarfile.open("ciberonline.tar.gz", "w:gz")
+	gzname = "log_" + sim_id + ".tar.gz"
+	json_gz = tarfile.open(gzname, "w:gz")
 	json_gz.add(file_name, arcname="ciberonline")
 	json_gz.close()
 
 	#save log to the end-point
-	data = {'simulation_identifier': sim_id, 'log_json': open("ciberonline.tar.gz", "rb")}
-	response = requests.post(HOST + "/api/v1/competitions/simulation_log/", data)
+	data = {'simulation_identifier': sim_id}
+	files = {'log_json': open(gzname, "rb")}
+	response = requests.post(HOST + "/api/v1/competitions/simulation_log/", data=data, files=files)
 
 	#print response.status_code
 	#print response.text
-	if response.status_code != 200:
-		print "Error posting to simulation log end-point!!"
-	else:
-		print "Log successfully posted, starter closing now.."
+	if response.status_code != 201:
+		print "ERROR: error posting log file to end point"
+		return
+
+
+	print "Log successfully posted, starter closing now.."
 
 	os.remove(file_name)
-	os.remove("ciberonline.tar.gz")
+	os.remove(gzname)
 
 	for key in tempFilesList:
 		tempFilesList[key].close()
+
 
 
 if __name__ == "__main__":
