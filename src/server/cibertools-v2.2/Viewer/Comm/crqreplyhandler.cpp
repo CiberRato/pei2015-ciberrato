825d61794ba6adc47edd74bdc5e1e6fa1c223333
Diogo Silva <dbtds@ua.pt>
2015-03-19 14:58:14 +0000

    Updated Viewer reply handler

--- src/server/cibertools-v2.2/Viewer/Comm/crqreplyhandler.cpp
+++ src/server/cibertools-v2.2/Viewer/Comm/crqreplyhandler.cpp
@@ -28,12 +28,7 @@ using namespace std;
 bool CRQReplyHandler::startDocument()
 {
 	replyObject = 0;
-    return TRUE;
-}
-
-/*---------------------------------------------------------------------------*/
-bool CRQReplyHandler::endDocument()
-{
+    type = UNKNOWN;
     return TRUE;
 }
 
@@ -43,7 +38,154 @@ bool CRQReplyHandler::startElement( const QString&, const QString&,
 {
 	/* process begin tag */
     const QString &tag = qName;
+    switch (type) {
+        case UNKNOWN:
+            if( tag == "Reply" )
+            {
+                replyObject = new CRReply; 
+                // process attributes
+                const QString status = attr.value( QString( "Status" ));
+                if ( !status.isNull() )
+                {
+                    if( status == "Ok" )
+                        replyObject->status = true;
+                    else
+                        replyObject->status = false;
+                }
+            } else if ( tag == "Parameters") {
+                type = PARAMETERS;
+                replyObject->parameters = new CRParameters();
 
+                const QString cycleTime = attr.value( QString( "CycleTime" ));
+                if( !cycleTime.isNull() )
+                    replyObject->parameters->cycleTime = cycleTime.toUInt();
+
+                const QString simTime = attr.value( QString( "SimTime" ));
+                if( !simTime.isNull() )
+                    replyObject->parameters->simTime = simTime.toUInt();
+
+                const QString compassTime = attr.value (QString( "CompassTime" ));
+                if( !compassTime.isNull() )
+                    replyObject->parameters->compassTime = compassTime.toUInt();
+
+                const QString obstacleNoise = attr.value(QString("ObstacleNoise"));
+                if( !obstacleNoise.isNull() )
+                    replyObject->parameters->obstacleNoise =
+                      obstacleNoise.toDouble();
+
+                const QString motorsNoise = attr.value (QString( "MotorsNoise" ));
+                if( !motorsNoise.isNull() )
+                    replyObject->parameters->motorsNoise = motorsNoise.toDouble();
+            }
+            else if ( tag == "Grid" )
+            {
+                type = GRID;    //Next time startElement will process one GRID
+                replyObject->grid = new CRGrid();
+            }
+            else if (tag == "Lab") {
+                type = LAB;
+                replyObject->lab = new CRLab();
+                // process attributes
+                const QString name = attr.value(QString("Name"));
+                if( !name.isNull() )
+                    replyObject->lab->setName( name.toAscii() );
+
+                const QString width = attr.value(QString("Width"));
+                if( !width.isNull() )
+                    replyObject->lab->setWidth( width.toFloat() );
+
+                const QString height = attr.value(QString("Height"));
+                if( !height.isNull() )
+                    replyObject->lab->setHeight( height.toFloat() );
+            }
+            break;
+        case PARAMETERS:
+
+            break;
+        case GRID:
+            if( tag == "Position" )
+            {
+                gridElement = new CRGridElement();
+                // Process attributs
+                const QString x = attr.value(QString("X"));
+                if (!x.isNull())
+                    gridElement->position.setX( x.toFloat() );
+
+                const QString y = attr.value(QString("Y"));
+                if (!y.isNull())
+                    gridElement->position.setY( y.toFloat() );
+
+                const QString dir = attr.value(QString("Dir"));
+                if (!dir.isNull())
+                    gridElement->direction = dir.toFloat();
+
+                replyObject->grid->addPosition( gridElement ); // Add one position to the grid
+            }
+            break;
+        case LAB:
+            if (tag == "Wall")
+            {
+                wall = new CRWall;
+                /* process attributes */
+                const QString height = attr.value(QString("Height"));
+                if (!height.isNull())
+                    wall->setWallHeight( height.toFloat() );
+            }
+            else if (tag == "Beacon")
+            {
+                vertice = new CRVertice;
+                beacon = new CRBeacon( *vertice );
+                /* process attributes */
+                const QString x = attr.value(QString("X"));
+                if (!x.isNull())
+                    vertice->setX( x.toFloat() );
+
+                const QString y = attr.value(QString("Y"));
+                if (!y.isNull())
+                    vertice->setY( y.toFloat() );
+
+                const QString height = attr.value(QString("Height"));
+                if (!height.isNull())
+                    replyObject->lab->addBeacon( *vertice, height.toFloat() );
+                else
+                    replyObject->lab->addBeacon( *vertice );
+            }
+
+            else if (tag == "Target")
+            {
+                target = new CRTarget;
+                vertice = new CRVertice;
+                /* process attributes */
+                const QString x = attr.value(QString("X"));
+                if (!x.isNull())
+                    vertice->setX( x.toFloat() );
+
+                const QString y = attr.value(QString("Y"));
+                if (!y.isNull())
+                    vertice->setY( y.toFloat() );
+
+                const QString radius = attr.value(QString("Radius"));
+                if (!radius.isNull())
+                    replyObject->lab->addTarget( *vertice, radius.toFloat() );
+                else
+                    replyObject->lab->addTarget( *vertice );
+            }
+            else if (tag == "Corner")
+            {
+                vertice = new CRVertice;
+                // process attributes
+                const QString x = attr.value(QString("X"));
+                if (!x.isNull())
+                    vertice->setX( x.toFloat() );
+
+                const QString y = attr.value(QString("Y"));
+                if (!y.isNull())
+                    vertice->setY( y.toFloat() );
+            }
+            break;
+    }
+    return true;
+/*
     if( tag == "Reply" )
     {
         replyObject = new CRReply;	// falta testar retorno
@@ -61,26 +203,15 @@ bool CRQReplyHandler::startElement( const QString&, const QString&,
     {
         replyObject->parameters = new CRParameters;	// falta testar retorno
         // process attributes
-        const QString cycleTime = attr.value( QString( "CycleTime" ));
-        if( !cycleTime.isNull() )
-            replyObject->parameters->cycleTime = cycleTime.toUInt();
+        
+    }
+    else if ( tag == "Grid" ) 
+    {
 
-        const QString simTime = attr.value( QString( "SimTime" ));
-        if( !simTime.isNull() )
-            replyObject->parameters->simTime = simTime.toUInt();
+    }
+    else if ( tag == "Lab" )
+    {
 
-        const QString compassTime = attr.value (QString( "CompassTime" ));
-        if( !compassTime.isNull() )
-            replyObject->parameters->compassTime = compassTime.toUInt();
-
-        const QString obstacleNoise = attr.value(QString("ObstacleNoise"));
-        if( !obstacleNoise.isNull() )
-            replyObject->parameters->obstacleNoise =
-              obstacleNoise.toDouble();
-
-        const QString motorsNoise = attr.value (QString( "MotorsNoise" ));
-        if( !motorsNoise.isNull() )
-            replyObject->parameters->motorsNoise = motorsNoise.toDouble();
     }
 	else
     {
@@ -88,7 +219,38 @@ bool CRQReplyHandler::startElement( const QString&, const QString&,
         return false;
     }
 
-	return true;
+	return true;*/
+}
+bool CRQReplyHandler::endElement( const QString&, const QString&, const
+                                QString& qName) {
+    const QString &tag = qName;
+    switch (type)
+    {
+        case UNKNOWN:
+        {
+            break;
+        }
+        case LAB:
+        {
+            if (tag == "Wall")
+                replyObject->lab->addWall( wall );
+            else if (tag == "Corner")
+                wall->addCorner( *vertice );
+
+            break;
+        }
+        case GRID:
+        {
+            // Does Nothing
+            break;
+        }
+    }
+    return true;
+}
+/*---------------------------------------------------------------------------*/
+bool CRQReplyHandler::endDocument()
+{
+    return true;
 }
 
 /*---------------------------------------------------------------------------*/
@@ -103,3 +265,7 @@ CRReply * CRQReplyHandler::reply()
 	return replyObject;
 }
 
+CRQReplyHandler::Type CRQReplyHandler::objectType()
+{
+    return type;
+}
