7a30473badd3a9eee9b9364db80393fab75a254f
Rafael Ferreira <mail@rafaelferreira.pt>
2015-03-24 10:55:22 +0000

    Code valid

--- src/web/competition/models.py
+++ src/web/competition/models.py
@@ -82,7 +82,7 @@ class Agent(models.Model):
     locations = models.CharField(max_length=256)
 
     language = models.CharField(choices=settings.ALLOWED_UPLOAD_LANGUAGES, max_length=100)
-    code_valid = models.BooleanField(default=False)
+    code_valid = models.BooleanField(default=True)
     is_virtual = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
