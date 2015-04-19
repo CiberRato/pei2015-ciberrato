f15526e7ef02d47d8c77e235fee44bfdfabbf4e2
Bruno Silva <brunomiguelsilva@ua.pt>
2015-04-19 16:02:38 +0100

    removed deamon part on starter it's now done by manager

--- src/server/starter.py
+++ src/server/starter.py
@@ -14,36 +14,6 @@ import zipfile
 from xml.dom import minidom
 
 class Starter:
-	def main(self):
-		# Loading settings
-		settings_str = re.sub("///.*", "", open("settings.json", "r").read())
-		settings = json.loads(settings_str)
-
-		END_POINT_HOST = settings["settings"]["starter_end_point_host"]
-		END_POINT_PORT = settings["settings"]["starter_end_point_port"]
-
-		print "[STARTER] Starter is in deamon mode"
-		end_point_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		end_point_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		end_point_tcp.bind((END_POINT_HOST, END_POINT_PORT))
-		end_point_tcp.listen(1)
-		end_point_c, end_point_add = end_point_tcp.accept()
-
-		# Waiting for a post to be done
-		data = None
-		while 1:
-			print "[STARTER] Waiting for simulation.."
-			while data == None or data == "":
-				data = end_point_c.recv(1024)
-			print "[STARTER] Received simulation with sim_id= " + data + ", starting now.."
-			self.run(data)
-			data = None
-
-		end_point_c.shutdown(socket.SHUT_RDWR)
-		end_point_c.close()
-		end_point_tcp.shutdown(socket.SHUT_RDWR)
-		end_point_tcp.close()
-
 	def run(self,sim_id):
 		# Find docker ip
 		DOCKERIP = None
