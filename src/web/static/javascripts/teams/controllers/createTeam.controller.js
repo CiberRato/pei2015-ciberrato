09e88b10fc1f67ff500afffd4fcf27fa77c0c0a9
Rodrigo Cunha <rodrigocunha@ua.pt>
2015-04-28 23:49:00 +0100

    dragon create team, and my grids only validated agents

--- src/web/static/javascripts/teams/controllers/createTeam.controller.js
+++ src/web/static/javascripts/teams/controllers/createTeam.controller.js
@@ -5,19 +5,20 @@
         .module('ciberonline.teams.controllers')
         .controller('CreateTeamController', CreateTeamController);
 
-    CreateTeamController.$inject = ['$location', '$routeParams', 'Authentication', 'Team'];
+    CreateTeamController.$inject = ['$location', '$routeParams', '$dragon', 'Authentication', 'Team'];
 
-    function CreateTeamController($location, $routeParams, Authentication, Team){
+    function CreateTeamController($location, $routeParams, $dragon, Authentication, Team){
         var vm = this;
 
         vm.create = create;
 
         var username;
+        var authenticatedAccount;
 
         activate();
 
         function activate(){
-            var authenticatedAccount = Authentication.getAuthenticatedAccount();
+            authenticatedAccount = Authentication.getAuthenticatedAccount();
             username = $routeParams.username;
 
             if(!authenticatedAccount){
@@ -38,6 +39,20 @@
             $.jGrowl("Team successfully created.", {
                 life: 2500,
                 theme: 'jGrowl-notification ui-state-highlight ui-corner-all success'
+            });
+            $dragon.onReady(function() {
+                swampdragon.open(function () {
+                    $dragon.subscribe('team', 'notifications', {
+                        'user': authenticatedAccount,
+                        'team': vm.name
+                    }, function (context, data) {
+                        // any thing that happens after successfully subscribing
+                        console.log("// any thing that happens after successfully subscribing");
+                    }, function (context, data) {
+                        // any thing that happens if subscribing failed
+                        console.log("// any thing that happens if subscribing failed");
+                    });
+                });
             });
             $location.path('/panel/'+ username + '/myTeams/');
 
