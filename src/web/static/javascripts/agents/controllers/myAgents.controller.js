ab5ffdd99fb138636c39acbb0009213ee574bc9c
Rodrigo Cunha <rodrigocunha@ua.pt>
2015-04-28 12:44:34 +0100

    notification code validation

--- src/web/static/javascripts/agents/controllers/myAgents.controller.js
+++ src/web/static/javascripts/agents/controllers/myAgents.controller.js
@@ -23,31 +23,7 @@
             function getByUserSuccessFn(data) {
                 vm.agents = data.data;
                 console.log(vm.agents);
-                $dragon.onReady(function() {
-                    swampdragon.open(function () {
-                       $dragon.onChannelMessage(function(channels, data) {
-                            /*
-                            if (data.data.message.status == 200){
-                                $.jGrowl(data.data.message.content, {
-                                    life: 3500,
-                                    theme: 'success'
-                                });
-                            }else if(data.data.message.status == 400){
-                                $.jGrowl(data.data.message.content, {
-                                    life: 3500,
-                                    theme: 'btn-danger'
-                                });
-                            }
-                            */
-                           if (data.data.message.trigger == ''){
 
-                           }
-                            console.log(channels);
-                            console.log(data.data._type);
-                            console.log(data.data.message);
-                        });
-                    });
-                });
             }
 
             function getByUserErrorFn(data) {
