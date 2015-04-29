1aae3b60fd6d3a9049cd209dba16a3d08d3aa5da
Bernardo Ferreira <Bernardo@aluno-4780.wireless.ua.pt>
2015-04-29 12:04:42 +0100

    Beacon

--- src/web/static/javascripts/logviewer/controllers/log.controller.js
+++ src/web/static/javascripts/logviewer/controllers/log.controller.js
@@ -76,12 +76,12 @@
             var i;
             if($scope.nBeacon==1){
                 ctx1.beginPath();
-                ctx1.arc($scope.beacon._X * $scope.zoom, $scope.beacon._Y * $scope.zoom, $scope.zoom + $scope.zoom/15, 0, 2*Math.PI);
+                ctx1.arc($scope.beacon._X * $scope.zoom, $scope.beacon._Y * $scope.zoom, $scope.zoom*2 + $scope.zoom/15, 0, 2*Math.PI);
                 ctx1.fillStyle = $scope.circleBorder;
                 ctx1.fill();
                 var imageObj = new Image();
                 imageObj.onload = function() {
-                    ctx1.drawImage(imageObj, $scope.beacon._X * $scope.zoom - $scope.zoom, $scope.beacon._Y * $scope.zoom - $scope.zoom, $scope.zoom*2,$scope.zoom*2 );
+                    ctx1.drawImage(imageObj, $scope.beacon._X * $scope.zoom - $scope.zoom, $scope.beacon._Y * $scope.zoom - $scope.zoom, $scope.zoom*4,$scope.zoom*2 );
                 };
                 imageObj.src = $scope.cheeseColor;
                 ctx1.fill();
