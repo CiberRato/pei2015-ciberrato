0691bcacd9088a4b44d4dd71972643e3e3330025
Rodrigo Cunha <rodrigocunha@ua.pt>
2015-05-04 18:20:40 +0100

    remove trials and solo trial

--- src/web/static/javascripts/soloTrials/controllers/soloByTeam.controller.js
+++ src/web/static/javascripts/soloTrials/controllers/soloByTeam.controller.js
@@ -6,12 +6,13 @@
         .module('ciberonline.soloTrials.controllers')
         .controller('SoloByTeamController', SoloByTeamController);
 
-    SoloByTeamController.$inject = ['SoloTrials', '$routeParams', 'Round'];
+    SoloByTeamController.$inject = ['SoloTrials', '$routeParams', '$timeout'];
 
-    function SoloByTeamController(SoloTrials, $routeParams, Round){
+    function SoloByTeamController(SoloTrials, $routeParams, $timeout){
         var vm = this;
         vm.competitionName = $routeParams.identifier;
         vm.teamName = $routeParams.teamName;
+        vm.deleteSoloTrial = deleteSoloTrial;
         activate();
 
         function activate(){
@@ -28,6 +29,37 @@
             }
 
 
+        }
+
+        function deleteSoloTrial(name){
+            SoloTrials.removeSoloTrial(name).then(removeSoloTrialSuccessFn, removeSoloTrialErrorFn);
+
+            function removeSoloTrialSuccessFn(){
+                $.jGrowl("Solo Trial has been removed successfully.", {
+                    life: 2500,
+                    theme: 'jGrowl-notification ui-state-highlight ui-corner-all success'
+                });
+                $timeout(function(){
+                    SoloTrials.getByTeam(vm.competitionName).then(getByTeamSuccessFn, getByTeamErrorFn);
+
+                    function getByTeamSuccessFn(data){
+                        vm.solos = data.data;
+                        console.log(vm.solos);
+                    }
+
+
+                    function getByTeamErrorFn(data){
+                        console.error(data.data);
+                    }                });
+            }
+
+            function removeSoloTrialErrorFn(data){
+                console.error(data.data);
+                $.jGrowl("Solo Trial could not be removed.", {
+                    life: 2500,
+                    theme: 'jGrowl-notification ui-state-highlight ui-corner-all danger'
+                });
+            }
         }
 
     }
