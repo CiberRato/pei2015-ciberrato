a153b619cbcda5f10bf851a653f28f480112da42
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-10 15:23:52 +0100

    Destroy the sticky note URL: ../api/v1/sticky_notes/crud/<identifier>/

--- src/web/stickynote/views.py
+++ src/web/stickynote/views.py
@@ -14,7 +14,7 @@ from competition.permissions import IsStaff
 
 
 class StickyNotesViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin, mixins.ListModelMixin,
-                         mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+                         mixins.UpdateModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = StickyNote.objects.filter(active=True)
     serializer_class = StickyNoteSerializer
 
@@ -70,50 +70,20 @@ class StickyNotesViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin, mixi
         serializer = self.serializer_class(sticky_note)
         return Response(serializer.data)
 
+
+
     def destroy(self, request, *args, **kwargs):
         """
-        B{Destroy} the grid positions
-        B{URL:} ../api/v1/competitions/grid_positions/<competition_name>/?team_name=<team_name>
+        B{Destroy} the sticky note
+        B{URL:} ../api/v1/sticky_notes/crud/<identifier>/
 
-        :type  competition_name: str
-        :param competition_name: The type of competition name
-        :type  team_name: str
-        :type  team_name: The team name
+        :type  identifier: str
+        :param identifier: The identifier
         """
-        competition = get_object_or_404(Competition.objects.all(), name=kwargs.get('pk', ''))
-
-        if competition.state_of_competition == 'Past':
-            return Response({'status': 'Bad Request',
-                             'message': 'The competition is in \'Past\' state.'},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-        team = get_object_or_404(Team.objects.all(), name=request.GET.get('team_name', ''))
-
-        if len(TeamMember.objects.filter(team=team, account=request.user)) != 1:
-            return Response({'status': 'Permission denied',
-                             'message': 'You must be part of the team.'},
-                            status=status.HTTP_403_FORBIDDEN)
-
-        team_enrolled = TeamEnrolled.objects.filter(team=team, competition=competition)
-        if len(team_enrolled) != 1:
-            return Response({'status': 'Permission denied',
-                             'message': 'Your team must be enrolled in the competition.'},
-                            status=status.HTTP_403_FORBIDDEN)
-
-        if not team_enrolled[0].valid:
-            return Response({'status': 'Permission denied',
-                             'message': 'Your team must be enrolled in the competition with valid inscription.'},
-                            status=status.HTTP_403_FORBIDDEN)
-
-        grid = get_object_or_404(GridPositions.objects.all(), competition=competition, team=team)
-
-        if grid.competition.type_of_competition.name == settings.PRIVATE_COMPETITIONS_NAME:
-            return Response({'status': 'Bad Request',
-                             'message': 'This grid can\'t be deleted!'},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-        grid.delete()
-
+        sticky_note = get_object_or_404(StickyNote.objects.all(), identifier=kwargs.get('pk', ''))
+        sticky_note.delete()
         return Response({'status': 'Deleted',
-                         'message': 'The grid positions has been deleted'},
+                         'message': 'The sticky note has been deleted'},
                         status=status.HTTP_200_OK)
+
+
