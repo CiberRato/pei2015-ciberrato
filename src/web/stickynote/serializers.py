763e4f438c09d28f7266c6f0626cc96fde977639
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-10 15:12:38 +0100

    models and serializer

---
+++ src/web/stickynote/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import StickyNote
+
+
+class StickyNoteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = StickyNote
+        fields = ('identifier', 'active', 'time', 'note', 'created_at', 'updated_at')
+        read_only_fields = ('identifier', 'created_at', 'updated_at')
