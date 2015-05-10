65c3cc5b8fff93832df3047d22cdd8a82a880a01
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-10 13:16:33 +0100

    dates problem solved (UTC+1) now only!

--- src/web/ciberonline/settings.py
+++ src/web/ciberonline/settings.py
@@ -92,13 +92,13 @@ SESSION_CACHE_ALIAS = "default"
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Lisbon'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
