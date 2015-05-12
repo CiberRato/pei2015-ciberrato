1b874d632c1ba7fed676a13dc13d5b34db58a571
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-12 11:23:24 +0100

    validation error message

--- src/web/authentication/validation.py
+++ src/web/authentication/validation.py
@@ -5,4 +5,5 @@ from django.http import Http404
 def test_captcha(hashkey, response):
     captcha = get_object_or_404(CaptchaStore.objects.all(), hashkey=hashkey)
     if captcha.response != response:
-        raise Http404('No %s matches the given query.' % queryset.model._meta.object_name)
+        captcha.delete()
+        raise Http404('The captcha response is wrong!')
