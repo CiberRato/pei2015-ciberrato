4be9fecc7ddcd8bf66836f6b27276405f2e5a4bd
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-12 18:35:50 +0100

    capthca

--- src/web/authentication/validation.py
+++ src/web/authentication/validation.py
@@ -2,8 +2,11 @@ from captcha.models import CaptchaStore
 from django.shortcuts import get_object_or_404
 from django.http import Http404
 
+
 def test_captcha(hashkey, response):
     captcha = get_object_or_404(CaptchaStore.objects.all(), hashkey=hashkey)
+    # print captcha.response
+    # print response
     if captcha.response != response:
         captcha.delete()
         raise Http404('The captcha response is wrong!')
