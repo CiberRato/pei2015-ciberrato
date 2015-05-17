1137ba921d8d52745210626c4c14edb174ede5e1
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-17 22:32:56 +0100

    sticky note done!

--- src/web/static/javascripts/stickyNotes/stickynote.jquery.js
+++ src/web/static/javascripts/stickyNotes/stickynote.jquery.js
@@ -2,16 +2,22 @@ $(document).ready(function(){
     $.getJSON("/api/v1/sticky_notes/active/", function(data) {
         console.log(data);
         var notes = data;
-        var i = 0;
+        var i = -1;
 
-        setTimeout(function changeNote() {
-            $(".side-note-container").html(notes[i].note);
-            if(notes.length-1 <= i){
-                i = 0;
-            }else{
-                i = i+1;
-            }
-            setTimeout(changeNote, notes[i].time*1000);
-        }, 0);
+        if(notes.length>1){
+            setTimeout(function changeNote() {
+                if(notes.length-1 <= i){
+                    i = 0;
+                }else{
+                    i = i+1;
+                }
+                $(".side-note-container").html(notes[i].note);
+                setTimeout(changeNote, notes[i].time*1000);
+            }, 0);
+        }else if(notes.length==1){
+            $(".side-note-container").html(notes[0].note);
+        }else{
+            $(".side-note-container").html("There are any note to show!");
+        }
     });
 });
