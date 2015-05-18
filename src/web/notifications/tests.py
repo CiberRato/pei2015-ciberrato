a44c59b050e4031374e0013c4c57132fff7729ca
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-18 19:22:42 +0100

    broadcasts unit tests notifications done urls added

--- src/web/notifications/tests.py
+++ src/web/notifications/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from authentication.models import Account
+from rest_framework.test import APIClient
+from .models import *
 
-# Create your tests here.
+
+class AuthenticationTestCase(TestCase):
+    def setUp(self):
+        Account.objects.create(email='test@test.com', username='test', first_name='unit', last_name='test',
+                               is_staff=True, teaching_institution='testUA')
+
+    def test_sticky_note(self):
+        user = Account.objects.get(email='test@test.com')
+        client = APIClient()
+        client.force_authenticate(user=user)
+
+        # create 10 broadcast notifications
+        for i in range(0, 11):
+            NotificationBroadcast.add(channel="broadcast", status="ok", message="ok"+str(i))
+
+        # get broadcast notifications
+        url = "/api/v1/notifications/broadcast/"
+        response = client.get(path=url)
+
+        i = 0
+        for response in response.data:
+            self.assertEqual(response["message"], u"{'status': 200, 'content': 'ok"+str(6+i)+"', 'trigger': ''}")
+            i += 1
+
+        client.force_authenticate(user=None)
