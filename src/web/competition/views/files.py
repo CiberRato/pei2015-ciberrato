c74a6af348af112b6155723b2ac7dac2f03c72ae
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-19 12:54:40 +0100

    test

--- src/web/competition/views/files.py
+++ src/web/competition/views/files.py
@@ -323,7 +323,8 @@ class UploadResourceFile(views.APIView):
         competition = get_object_or_404(Competition.objects.all(), name=competition_name)
         r = get_object_or_404(Round.objects.all(), name=round_name, parent_competition=competition)
 
-        if getattr(r, param + "_path", None) is not None and getattr(r, param + "_can_delete"):
+        if getattr(r, param + "_path", None) is not None and len(getattr(r, param + "_path", None)) != 0\
+                and getattr(r, param + "_can_delete"):
             default_storage.delete(getattr(r, param + "_path", None))
 
         setattr(r, param + "_path", default_storage.path(path))
