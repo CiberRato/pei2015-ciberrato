b32bff55c5aa2b49cdf5e84c011e40846e2d234c
Rodrigo Cunha <rodrigocunha@ua.pt>
2015-05-20 18:28:26 +0100

    challenges

--- src/web/static/javascripts/hallOfFame/controllers/allChallenges.controller.js
+++ src/web/static/javascripts/hallOfFame/controllers/allChallenges.controller.js
@@ -5,11 +5,16 @@
         .module('ciberonline.hallOfFame.controllers')
         .controller('AllChallengesController', AllChallengesController);
 
-    AllChallengesController.$inject = ['$scope', 'Competition', '$timeout', 'Round'];
+    AllChallengesController.$inject = ['$scope', 'Competition', '$timeout', 'Round', 'Authentication', 'Agent', 'HallOfFame'];
 
-    function AllChallengesController($scope, Competition, $timeout, Round){
+    function AllChallengesController($scope, Competition, $timeout, Round, Authentication, Agent, HallOfFame){
         var vm = this;
         vm.deleteChallenge = deleteChallenge;
+        var authenticatedAccount = Authentication.getAuthenticatedAccount();
+        vm.username = authenticatedAccount.username;
+        vm.launchTrial = launchTrial;
+        vm.getGrid = getGrid;
+        vm.getLab = getLab;
 
         activate();
 
@@ -22,11 +27,24 @@
 
             function getHallOfFameSuccessFn(data){
                 vm.challenges = data.data;
+                for(var i = 0; i<vm.challenges.length; i++){
+                    getFiles(vm.challenges[i].name, i);
+                }
+
                 console.log(vm.challenges);
 
-                $scope.loader = {
-                    loading: true
-                };
+                Agent.getByUser(vm.username).then(getAgentsSuccessFn, getAgentsErrorFn);
+                function getAgentsSuccessFn(data){
+                    vm.agents = data.data;
+                    $scope.loader = {
+                        loading: true
+                    };
+                }
+
+                function getAgentsErrorFn(data){
+                    console.error(data.data);
+                }
+
             }
 
             function getHallOfFameErrorFn(data){
@@ -35,6 +53,18 @@
 
 
 
+        }
+
+        function getFiles(name, i){
+            Round.getFiles(name, "Hall of fame - Single").then(getFilesSuccessFn, getFilesErrorFn);
+
+            function getFilesSuccessFn(data){
+                vm.challenges[i].files = data.data;
+            }
+
+            function getFilesErrorFn(data){
+                console.error(data.data);
+            }
         }
 
 
@@ -70,6 +100,80 @@
                     theme: 'jGrowl-notification ui-state-highlight ui-corner-all danger'
                 });
             }
+        }
+
+        function launchTrial(roundName){
+            var tmp = document.getElementById('select').value;
+            var agent = tmp.substr(0,tmp.indexOf(','));
+            var team = tmp.substr(tmp.indexOf(',') + 1);
+
+            HallOfFame.launchTrial(roundName, agent, team).then(launchTrialSuccessFn, launchTrialErrorFn);
+
+            function launchTrialSuccessFn(data){
+                $.jGrowl("Trial has been created successfully", {
+                    life: 2500,
+                    theme: 'jGrowl-notification ui-state-highlight ui-corner-all success'
+                });
+                console.log(data.data);
+                $timeout(function(){
+                    Round.getTrials(roundName, "Hall of fame - Single").then(getTrialsSuccessFn, getTrialsErrorFn);
+
+                    function getTrialsSuccessFn(data){
+                        vm.trials = data.data;
+                        console.log(vm.trials);
+
+                        Agent.getByUser(vm.username).then(getAgentsSuccessFn, getAgentsErrorFn);
+
+                        function getAgentsSuccessFn(data){
+                            vm.agents = data.data;
+                            $scope.loader = {
+                                loading: true
+                            };
+                        }
+
+                        function getAgentsErrorFn(data){
+                            console.error(data.data);
+                        }
+
+
+                    }
+
+                    function getTrialsErrorFn(data) {
+                        console.error(data.data);
+                    }
+                });
+            }
+
+            function launchTrialErrorFn(data){
+                console.error(data.data);
+                $.jGrowl(data.data.message, {
+                    life: 5000,
+                    theme: 'jGrowl-notification ui-state-highlight ui-corner-all danger'
+                });
+            }
+        }
+
+        function getGrid(grid){
+
+            SoloTrials.getResource(grid).then(getResourceSuccessFn, getResourceErrorFn);
+
+            function getResourceSuccessFn(data) {
+                vm.grid = data.data;
+            }
+        }
+
+        function getLab(lab){
+
+            SoloTrials.getResource(lab).then(getResourceSuccessFn, getResourceErrorFn);
+
+            function getResourceSuccessFn(data){
+                vm.lab = data.data;
+            }
+
+        }
+
+        function getResourceErrorFn(data){
+            console.error(data.data);
         }
 
     }
--- src/web/static/javascripts/hallOfFame/controllers/challengeDetail.controller.js
+++ src/web/static/javascripts/hallOfFame/controllers/challengeDetail.controller.js
@@ -5,14 +5,13 @@
         .module('ciberonline.hallOfFame.controllers')
         .controller('ChallengeDetailController', ChallengeDetailController);
 
-    ChallengeDetailController.$inject = ['$scope', '$timeout', 'Round', '$routeParams', 'Authentication', 'Agent', 'HallOfFame'];
+    ChallengeDetailController.$inject = ['$scope', 'Round', '$routeParams', 'Authentication'];
 
-    function ChallengeDetailController($scope, $timeout, Round, $routeParams, Authentication, Agent, HallOfFame){
+    function ChallengeDetailController($scope, Round, $routeParams, Authentication){
         var vm = this;
         vm.roundName = $routeParams.name;
         var authenticatedAccount = Authentication.getAuthenticatedAccount();
         vm.username = authenticatedAccount.username;
-        vm.launchTrial = launchTrial;
 
         activate();
 
@@ -34,20 +33,9 @@
                     getAgent(vm.trials[i].identifier, i);
                 }
 
-
-                Agent.getByUser(vm.username).then(getAgentsSuccessFn, getAgentsErrorFn);
-
-                function getAgentsSuccessFn(data){
-                    vm.agents = data.data;
-                    $scope.loader = {
-                        loading: true
-                    };
-                }
-
-                function getAgentsErrorFn(data){
-                    console.error(data.data);
-                }
-
+                $scope.loader = {
+                    loading: true
+                };
 
             }
 
@@ -71,56 +59,7 @@
         }
 
 
-        function launchTrial(){
-            var tmp = document.getElementById('select').value;
-            var agent = tmp.substr(0,tmp.indexOf(','));
-            var team = tmp.substr(tmp.indexOf(',') + 1);
-            console.log(team);
-            HallOfFame.launchTrial(vm.roundName, agent, team).then(launchTrialSuccessFn, launchTrialErrorFn);
 
-            function launchTrialSuccessFn(data){
-                $.jGrowl("Trial has been created successfully", {
-                    life: 2500,
-                    theme: 'jGrowl-notification ui-state-highlight ui-corner-all success'
-                });
-                console.log(data.data);
-                $timeout(function(){
-                    Round.getTrials(vm.roundName, "Hall of fame - Single").then(getTrialsSuccessFn, getTrialsErrorFn);
-
-                    function getTrialsSuccessFn(data){
-                        vm.trials = data.data;
-                        console.log(vm.trials);
-
-                        Agent.getByUser(vm.username).then(getAgentsSuccessFn, getAgentsErrorFn);
-
-                        function getAgentsSuccessFn(data){
-                            vm.agents = data.data;
-                            $scope.loader = {
-                                loading: true
-                            };
-                        }
-
-                        function getAgentsErrorFn(data){
-                            console.error(data.data);
-                        }
-
-
-                    }
-
-                    function getTrialsErrorFn(data) {
-                        console.error(data.data);
-                    }
-                });
-            }
-
-            function launchTrialErrorFn(data){
-                console.error(data.data);
-                $.jGrowl(data.data.message, {
-                    life: 5000,
-                    theme: 'jGrowl-notification ui-state-highlight ui-corner-all danger'
-                });
-            }
-        }
 
     }
 })();
