18b36ea609a75dd64deccaf3ea705bb5ebc98b82
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-23 22:02:28 +0100

    tcp receiver

--- src/web/notifications/management/commands/stream.py
+++ src/web/notifications/management/commands/stream.py
@@ -1,10 +1,39 @@
 from django.core.management.base import BaseCommand, CommandError
 from ...models import StreamTrial
+import socket
 
 
 class Command(BaseCommand):
     help = 'This is a TCP receiver from the simulator viewer!'
 
     def handle(self, *args, **options):
-        self.stdout.write('# The script was launched!')
-        StreamTrial.add(trial_identifier="9690b514-0895-4091-93ee-c902ac8f872f", message="teste!")
+        self.stdout.write('[STREAM] The script was launched!')
+
+        while True:
+            self.stdout.write('[STREAM] Waiting for new streaming!')
+
+            host = "127.0.0.1"
+            port = 10000
+
+            sim_viewer_connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sim_viewer_connect.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sim_viewer_connect.bind((host, port))
+            sim_viewer_connect.listen(1)
+            sim_viewer, addr = sim_viewer_connect.accept()
+
+            identifier = sim_viewer.recv(1024)
+            self.stdout.write('[STREAM] Trial ' + str(identifier) + ' is now streaming!')
+
+            while True:
+                data = sim_viewer.recv(1024 * 8)
+                if str(data) == "END":
+                    break
+
+                StreamTrial.add(trial_identifier=identifier, message=data)
+
+            sim_viewer_connect.shutdown(socket.SHUT_RDWR)
+            sim_viewer_connect.close()
+            sim_viewer.shutdown(socket.SHUT_RDWR)
+            sim_viewer.close()
+            
+            self.stdout.write('[STREAM] Trial ' + str(identifier) + ' ended the streaming!')
