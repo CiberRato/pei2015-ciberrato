cc129de8c5d23492f9049a46ca0dcbdb3f0c8a20
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-24 21:53:14 +0100

    fixed post log error

--- src/web/static/javascripts/notifications/services/notifications.service.js
+++ src/web/static/javascripts/notifications/services/notifications.service.js
@@ -15,31 +15,35 @@
         return Notification;
 
         function activateNotifications(){
-            $dragon.onChannelMessage(function(channels, data) {
-                console.log("HOME");
-                if(data.data._type != 'streamtrial'){
-                    if (data.data.message.status == 200){
-                        $.jGrowl(data.data.message.content, {
-                            life: 3500,
-                            theme: 'jGrowl-notification ui-state-highlight ui-corner-all success'
-                        });
-                    }else if(data.data.message.status == 400){
-                        $.jGrowl(data.data.message.content, {
-                            life: 3500,
-                            theme: 'jGrowl-notification ui-state-highlight ui-corner-all danger'
-                        });
-                    }else if(data.data.message.status == 100){
-                        $.jGrowl(data.data.message.content, {
-                            life: 3500,
-                            theme: 'jGrowl-notification ui-state-highlight ui-corner-all info'
-                        });
-                    }
-                    // console.log(channels);
-                    console.log(data.data._type);
-                    console.log(data.data.message);
-                }else{
-                    // tratar aqui do stream
-                }
+            $dragon.onReady(function() {
+                swampdragon.open(function () {
+                    $dragon.onChannelMessage(function (channels, data) {
+                        console.log("HOME");
+                        if (data.data._type != 'streamtrial') {
+                            if (data.data.message.status == 200) {
+                                $.jGrowl(data.data.message.content, {
+                                    life: 3500,
+                                    theme: 'jGrowl-notification ui-state-highlight ui-corner-all success'
+                                });
+                            } else if (data.data.message.status == 400) {
+                                $.jGrowl(data.data.message.content, {
+                                    life: 3500,
+                                    theme: 'jGrowl-notification ui-state-highlight ui-corner-all danger'
+                                });
+                            } else if (data.data.message.status == 100) {
+                                $.jGrowl(data.data.message.content, {
+                                    life: 3500,
+                                    theme: 'jGrowl-notification ui-state-highlight ui-corner-all info'
+                                });
+                            }
+                            // console.log(channels);
+                            console.log(data.data._type);
+                            console.log(data.data.message);
+                        } else {
+                            // tratar aqui do stream
+                        }
+                    });
+                });
             });
         }
     }
