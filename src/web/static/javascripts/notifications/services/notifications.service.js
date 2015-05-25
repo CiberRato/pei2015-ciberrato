f90dda17a8f44203212454cae54df1431003955e
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-25 18:13:30 +0100

    2 slots for events

--- src/web/static/javascripts/notifications/services/notifications.service.js
+++ src/web/static/javascripts/notifications/services/notifications.service.js
@@ -6,50 +6,98 @@
         .factory("Notification", Notification);
 
     function Notification(){
-        var events = (function(){
-          var topics = {};
-          var hOP = topics.hasOwnProperty;
+          var events = (function(){
+          var slotTopics0 = {};
+          var slotTopics1 = {};
+          var hOP0 = slotTopics0.hasOwnProperty;
+          var hOP1 = slotTopics1.hasOwnProperty;
 
-          var hasSubscribed = function(topicSubscribed){
-            for (var topic in topics){
-              if(topics.hasOwnProperty(topic)){
-                if(topic==topicSubscribed){
-                  return true;
+          var hasSubscribed = function(topicSubscribed, slot){
+            if(slot==0){
+              for (var topic0 in slotTopics0){
+                if(slotTopics0.hasOwnProperty(topic0)){
+                  if(topic0==topicSubscribed){
+                    return true;
+                  }
                 }
               }
+              return false;
+            }else if (slot==1) {
+              for (var topic1 in slotTopics1){
+                if(slotTopics1.hasOwnProperty(topic1)){
+                  if(topic1==topicSubscribed){
+                    return true;
+                  }
+                }
+              }
+              return false;
+            }else{
+              return true;
             }
-            return false;
           };
 
           return {
-            subscribe: function(topic, listener) {
-              if(hasSubscribed(topic)){
-                return {
-                  remove: function() {
-                    delete topics[topic][0];
-                  }
-                };
+            subscribe: function(topic, slot, listener) {
+              if(hasSubscribed(topic, slot)){
+                if(slot==0){
+                  return {
+                    remove: function() {
+                      delete slotTopics0[topic][0];
+                    }
+                  };
+                }else if (slot==1) {
+                  return {
+                    remove: function() {
+                      delete slotTopics1[topic][0];
+                    }
+                  };
+                }
               }else{
-                // Create the topic's object if not yet created
-                if(!hOP.call(topics, topic)) topics[topic] = [];
+                if(slot==0){
+                  // Create the topic's object if not yet created
+                  if(!hOP0.call(slotTopics0, topic)) slotTopics0[topic] = [];
 
-                // Add the listener to queue
-                var index = topics[topic].push(listener) -1;
+                  // Add the listener to queue
+                  var index = slotTopics0[topic].push(listener) -1;
 
-                // Provide handle back for removal of topic
-                return {
-                  remove: function() {
-                    delete topics[topic][index];
-                  }
-                };
+                  // Provide handle back for removal of topic
+                  return {
+                    remove: function() {
+                      delete slotTopics0[topic][index];
+                    }
+                  };
+                }else if (slot==1) {
+                  // Create the topic's object if not yet created
+                  if(!hOP1.call(slotTopics1, topic)) slotTopics1[topic] = [];
+
+                  // Add the listener to queue
+                  var index = slotTopics1[topic].push(listener) -1;
+
+                  // Provide handle back for removal of topic
+                  return {
+                    remove: function() {
+                      delete slotTopics1[topic][index];
+                    }
+                  };
+                }
               }
             },
             publish: function(topic, info) {
+              // slotTopics0
               // If the topic doesn't exist, or there's no listeners in queue, just leave
-              if(!hOP.call(topics, topic)) return;
+              if(!hOP0.call(slotTopics0, topic)) return;
 
               // Cycle through topics queue, fire!
-              topics[topic].forEach(function(item) {
+              slotTopics0[topic].forEach(function(item) {
+                    item(info != undefined ? info : {});
+              });
+
+              // slotTopics1
+              // If the topic doesn't exist, or there's no listeners in queue, just leave
+              if(!hOP1.call(slotTopics1, topic)) return;
+
+              // Cycle through topics queue, fire!
+              slotTopics1[topic].forEach(function(item) {
                     item(info != undefined ? info : {});
               });
             }
