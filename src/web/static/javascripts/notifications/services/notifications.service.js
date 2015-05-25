99d12b712a1d8b5ddd15492c8cad170458c4e44a
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-25 17:38:13 +0100

    subscribe only one topic

--- src/web/static/javascripts/notifications/services/notifications.service.js
+++ src/web/static/javascripts/notifications/services/notifications.service.js
@@ -12,20 +12,39 @@
           var topics = {};
           var hOP = topics.hasOwnProperty;
 
+          var hasSubscribed = function(topicSubscribed){
+            for (var topic in topics){
+              if(topics.hasOwnProperty(topic)){
+                if(topic==topicSubscribed){
+                  return true;
+                }
+              }
+            }
+            return false;
+          };
+
           return {
             subscribe: function(topic, listener) {
-              // Create the topic's object if not yet created
-              if(!hOP.call(topics, topic)) topics[topic] = [];
+              if(hasSubscribed(topic)){
+                return {
+                  remove: function() {
+                    delete topics[topic][0];
+                  }
+                };
+              }else{
+                // Create the topic's object if not yet created
+                if(!hOP.call(topics, topic)) topics[topic] = [];
 
-              // Add the listener to queue
-              var index = topics[topic].push(listener) -1;
+                // Add the listener to queue
+                var index = topics[topic].push(listener) -1;
 
-              // Provide handle back for removal of topic
-              return {
-                remove: function() {
-                  delete topics[topic][index];
-                }
-              };
+                // Provide handle back for removal of topic
+                return {
+                  remove: function() {
+                    delete topics[topic][index];
+                  }
+                };
+              }
             },
             publish: function(topic, info) {
               // If the topic doesn't exist, or there's no listeners in queue, just leave
