f4fff2c7f9f5bdab99d6ca0a30b7eaad0d54d82f
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-26 19:42:55 +0100

    wsgi prod

---
+++ src/web/ciberonline/wsgi_prod.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for ciberonline project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ciberonline.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
