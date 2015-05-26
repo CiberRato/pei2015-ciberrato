d4bb67c3d453b4bf7fc1ac88ef8ba34914064967
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-26 16:55:48 +0100

    deploy

--- src/web/start-deploy.py
+++ src/web/start-deploy.py
@@ -2,8 +2,8 @@ import subprocess
 
 
 if __name__ == "__main__":
-    dragon = subprocess.Popen(["python", "dragon.py", "0.0.0.0:9999"])
-    django = subprocess.Popen(["python", "manage.py", "runserver", "0.0.0.0:8000"])
+    dragon = subprocess.Popen(["python", "dragon.py", "0.0.0.0:8081"])
+    django = subprocess.Popen(["python", "manage.py", "runserver", "0.0.0.0:8080"])
     stream = subprocess.Popen(["python", "manage.py", "stream"])
 
     dragon.wait()
