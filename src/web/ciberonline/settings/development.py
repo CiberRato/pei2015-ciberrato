44c8280ca7b1378686ce190b14ff57535a70f10e
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-26 17:22:27 +0100

    dragon

--- src/web/ciberonline/settings/development.py
+++ src/web/ciberonline/settings/development.py
@@ -26,5 +26,4 @@ PREPARE_SIM_ENDPOINT = "http://127.0.0.1:9000/api/v1/trial_id/"
 TEST_CODE_ENDPOINT = "http://127.0.0.1:9000/api/v1/test_agent/?agent_name=<agent_name>&team_name=<team_name>/"
 
 # swampdragon settings
-SWAMP_DRAGON_HOST = 'ciber.atnog.org'
-SWAMP_DRAGON_PORT = 8081
+DRAGON_URL = 'http://ciber.atnog.org:8081/'
