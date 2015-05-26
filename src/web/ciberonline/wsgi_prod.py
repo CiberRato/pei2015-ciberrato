38b0e261ae4fe91f0bee8c7b4096dccc43fc115a
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-26 19:50:22 +0100

    wsgi prod

--- src/web/ciberonline/wsgi_prod.py
+++ src/web/ciberonline/wsgi_prod.py
@@ -10,8 +10,10 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 import sys
 
+print os.path.abspath(os.path.join(os.path.dirname(__file__)))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ciberonline.settings")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ciberonline.settings'
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
