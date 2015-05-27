b379fb67a037d2f9f032a1942ce8e506594e5405
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-27 19:37:02 +0100

    send email with postfix

--- src/web/ciberonline/settings/base.py
+++ src/web/ciberonline/settings/base.py
@@ -35,6 +35,14 @@ EMAIL_USE_SSL = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 """
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = 'CIBER RATO <ciber@ciberrato.vm.atnog.av.it.pt>'
+
 # Application definition
 INSTALLED_APPS = (
     'django.contrib.admin',
