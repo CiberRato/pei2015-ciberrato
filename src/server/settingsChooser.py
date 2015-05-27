7671cddd8d2d77926a129e90f85e416c8f27e9d7
Bruno Silva <brunomiguelsilva@ua.pt>
2015-05-27 14:57:14 +0100

    added file to automatically load the correct settings

---
+++ src/server/settingsChooser.py
@@ -0,0 +1,19 @@
+import re
+import json
+from socket import gethostname
+
+class Settings():
+	def __init__(self):
+		self.serverNames = ["ciber"]
+
+	def getSettings(self):
+		server_name = gethostname()
+		print server_name
+
+		for n in self.serverNames:
+			if server_name == n:
+				settings_str = re.sub("///.*", "", open("settings.json", "r").read())
+				return json.loads(settings_str)
+
+		settings_str = re.sub("///.*", "", open("settings-dev.json", "r").read())
+		return json.loads(settings_str)
