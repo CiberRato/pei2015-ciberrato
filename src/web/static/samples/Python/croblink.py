482e89c6a477e6f2c4370a3d2fb26addeb979047
Rafael Ferreira <mail@rafaelferreira.pt>
2015-06-07 20:02:34 +0100

    LABI JPB samples

--- src/web/static/samples/Python/croblink.py
+++ src/web/static/samples/Python/croblink.py
@@ -1,34 +1,40 @@
+# encoding=utf-8
 #
 # Authors: Nuno Lau and Luis Seabra Lopes
 # October - November 2013
+#
+# Simplified by João Paulo Barraca
+# May 2015
 #
 
 import socket
 NUM_IR_SENSORS = 4
 
-
-class CRobLink:
+class CRobotLink:
     """
     Class used to simulate the robot.
     """
-    def __init__(self, rob_name, rob_id, host):
+    def __init__(self, name, identifier, host):
         """
         Initializes robot and connects to simulator on a given host.
         It registers the robot on the simulator sending a message TCP
             <Robot Id='robId' Name='robName' />
 
         Parameters:
-            `rob_name`: Robot name that will be used by the simulator
-            `rob_id`: The robot identifier (it must be unique for the
+            `name`: Robot name that will be used by the simulator
+            `identifier`: The robot identifier (it must be unique for the
                 simulation)
             `host`: Hostname of the simulator (might include port,
                 example: 127.0.0.1:4001)
         """
-        self.rob_name = rob_name
-        self.rob_id = rob_id
+        self.name = name
+        self.identifier = identifier
+        self.pending_sensors = ""
+        self.pending_motors = ""
 
         val = host.split(":")
         port_conn = 6000
+
         if len(val) > 1:
             self.host = val[0]
             port_conn = int(val[1])
@@ -37,9 +43,10 @@ class CRobLink:
 
         self.sock = socket.socket(socket.AF_INET,  # Internet
                                   socket.SOCK_STREAM)  # UDP
+        self.sock.settimeout(30)
         self.sock.connect((self.host, port_conn))
         self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-        msg = '<Robot Id="'+str(rob_id)+'" Name="'+rob_name+'" />\x04'
+        msg = '<Robot Id="%s" Name="%s" />\x04' % (str(identifier), name)
 
         self.sock.send(msg)
         data = self.sock.recv(1024)
@@ -56,69 +63,76 @@ class CRobLink:
         sax.parseString(d2, handler)
         self.status = handler.status
 
+    def request_sensors(self, sensors):
+        if len(sensors) == 0:
+            return
+
+        s = "<SensorRequests "
+        for sensor in sensors:
+            if sensor == "Beacon":
+                s += " Beacon0=\"Yes\""
+            elif sensor == "IRSensor":
+                s += " IRSensor0=\"Yes\"  IRSensor1=\"Yes\" IRSensor2=\"Yes\" IRSensor3=\"Yes\""
+            else:
+                s += " %s=\"Yes\"" % sensor
+        s += "/>"
+        self.pending_sensors = s
+
     def read_sensors(self):
         """
         This functions read all the sensors available given by the
             simulator, storing them at measures field.
 
         Measures field (CMeasures object) is composed by:
-            compassReady    * True if compass value is available to be
-                                read
             compass         * Compass value (value between 0 and 360)
-            irSensorReady   * True if sensor is available (list of
-                                sensors - NUM_IR_SENSORS)
-            irSensor        * Sensor value (the more closer to the
+            ir_sensor        * Sensor value (the more closer to the
                                 obstacle, the bigger is the value)
-            beaconReady     * True if beacon is available to be read
-            beacon          * Returns a tuple of (bool, float) where
-                                the bool represents if the Beacon is
-                                visible and the float represents the
-                                distance when visible
+            beacon          * Returns the distance to the Beacon
+                                if visible
             time = 0        * Current time of the simulation
-            groundReady     * True if ground sensor is available to
-                                be read
             ground          * True if robot detects the ground as a
                                 target area
-            collisionReady  * True if collision sensor is available
-                                to be read
             collision       * True if robot as collided
             start           * True if the button start was pressed
             stop            * True if the button end was pressed
-            endLed          * True when the end led has been turned
-            returningLed    * True when the returning led has been
+            end_led          * True when the end led has been turned
+            returning_led    * True when the returning led has been
                                 turned on
-            visitingLed     * True when the visiting led has been
+            visiting_led     * True when the visiting led has been
                                 turned on
-            gpsReady        * True when GPS is available
-            gpsDirReady     * True when GPS direction is available
-            x               * Current x-axis position of the robot,
+            gps_x           * Current x-axis position of the robot,
                                 GPS must be enabled
-            y               * Current y-axis position of the robot,
+            gps_y           * Current y-axis position of the robot,
                                 GPS must be enabled
-            dir             * Current direction of the robot, GPS
+            direction       * Current direction of the robot, GPS
                                 must be enabled
-            scoreReady      * Simulation score is ready to be read
             score           * Returns the current score of the robot
-            arrivalTimeReady    * True when available to be read
-            arrivalTime         * Current arrival time of the robot
-            returningTimeReady  * True when available to be read
-            returningTime       * Current returning time of the robot
-            collisionsReady     * True when available to be read
-            collisions          * Returns the number of collisions
-            hearMessage         * Used to communicate between robots
+            arrival_time    * Current arrival time of the robot
+            returning_time  * Current returning time of the robot
+            collisions      * Returns the number of collisions
+            hear_message    * Used to communicate between robots
+
+            All fields can have a value or be equal to None if the
+            item is not available.
         """
-        data = self.sock.recv(4096)
-        d2 = data.split('\x04')[0]
+        data = ""
+        try:
+            data = self.sock.recv(4096).strip()
+            #print "Received : \n%s" % data
 
-        # print "Received : \"" + d2 +'"'
-        parser = sax.make_parser()
-        # Tell it what handler to use
-        handler = StructureHandler()
-        parser.setContentHandler(handler)
+            d2 = data.split('\x04')[0]
 
-        sax.parseString(d2, handler)
-        self.status = handler.status
-        self.measures = handler.measures
+            parser = sax.make_parser()
+            # Tell it what handler to use
+            handler = StructureHandler()
+            parser.setContentHandler(handler)
+
+            sax.parseString(d2, handler)
+            self.status = handler.status
+            self.measures = handler.measures
+        except:
+            print "ERROR Parsing server response: %s" % data
+            pass
 
     def sync_robot(self):
         """
@@ -130,7 +144,16 @@ class CRobLink:
         synchronous mode, otherwise it will run every cycle till the
         real cycle time (takes longer).
         """
+
+        msg = "<Actions %s>%s</Actions>\x04" % (self.pending_motors, self.pending_sensors)
+        #print "Sending: \n %s" % msg
+
+        self.sock.send(msg)
+        self.pending_motors = ""
+        self.pending_sensors = ""
         msg = '<Actions> <Sync/> </Actions>\x04'
+
+        #print "Sending: \n %s" % msg
         self.sock.send(msg)
 
     def drive_motors(self, lpow, rpow):
@@ -142,9 +165,8 @@ class CRobLink:
             `lpow` - Speed for the left motor (value between 0 and 1)
             `rpow` - Speed for the right motor (value between 0 and 1)
         """
-        msg = '<Actions LeftMotor="' + \
-            str(lpow)+'" RightMotor="'+str(rpow)+'"/>\x04'
-        self.sock.send(msg)
+        self.pending_motors = 'LeftMotor="' + \
+            str(lpow)+'" RightMotor="'+str(rpow)+'"'
 
     def set_returning_led(self, val):
         """
@@ -154,6 +176,7 @@ class CRobLink:
         Parameters:
             `val` - True to set the led on, False otherwise
         """
+
         msg = '<Actions LeftMotor="0.0" RightMotor="0.0" ' + \
             'ReturningLed="' + ("On" if val else "Off") + '"/>\x04'
         self.sock.send(msg)
@@ -179,43 +202,36 @@ class CRobLink:
             'EndLed="On"/>\x04'
         self.sock.send(msg)
 
-
 class CMeasures:
     """
     Structure where the values read from the simulator are stored.
     """
     def __init__(self):
-        self.compassReady = False
-        self.compass = 0.0
-        self.irSensorReady = [False for i in range(NUM_IR_SENSORS)]
-        self.irSensor = [0.0 for i in range(NUM_IR_SENSORS)]
-        self.beaconReady = False
-        self.beacon = (False, 0.0)
-        self.time = 0
+        self.compass = None
+        self.ir_sensor = [None for i in range(NUM_IR_SENSORS)]
+        self.ir_sensor_center = None
+        self.ir_sensor_left = None
+        self.ir_sensor_right = None
+        self.ir_sensor_back = None
 
-        self.groundReady = False
-        self.ground = False
-        self.collisionReady = False
-        self.collision = False
-        self.start = False
-        self.stop = False
-        self.endLed = False
-        self.returningLed = False
-        self.visitingLed = False
-        self.x = 0.0
-        self.y = 0.0
-        self.dir = 0.0
+        self.beacon = None
+        self.time = None
 
-        self.scoreReady = False
-        self.score = 100000
-        self.arrivalTimeReady = False
-        self.arrivalTime = 10000
-        self.returningTimeReady = False
-        self.returningTime = 10000
-        self.collisionsReady = False
-        self.collisions = 0
-        self.gpsReady = False
-        self.gpsDirReady = False
+        self.ground = None
+        self.collision = None
+        self.start = None
+        self.stop = None
+        self.end_led = None
+        self.returning_led = None
+        self.visiting_led = None
+        self.x = None
+        self.y = None
+        self.dir = None
+
+        self.score = None
+        self.arrival_time = None
+        self.returning_time = None
+        self.collisions = None
 
         self.hearMessage = ''
 
@@ -244,75 +260,69 @@ class StructureHandler(sax.ContentHandler):
         elif name == "Measures":
             self.measures.time = int(attrs["Time"])
         elif name == "Sensors":
-            self.measures.compassReady = ("Compass" in attrs.keys())
-            if self.measures.compassReady:
+            if "Compass" in attrs:
                 self.measures.compass = float_of_string(attrs["Compass"])
 
-            self.measures.collisionReady = ("Collision" in attrs.keys())
-            if self.measures.collisionReady:
+            if "Collision" in attrs:
                 self.measures.collision = (attrs["Collision"] == "Yes")
 
-            self.measures.groundReady = ("Ground" in attrs.keys())
-            if self.measures.groundReady:
+            if "Ground" in attrs:
                 self.measures.ground = int(attrs["Ground"])
 
         elif name == "IRSensor":
             id = int(attrs["Id"])
             # print "IRSensor id ", id
             if id < NUM_IR_SENSORS:
-                self.measures.irSensorReady[id] = True
-                # print "IRSensor val ", attrs["Value"]
-                self.measures.irSensor[id] = float_of_string(attrs["Value"])
+                if id == 0:
+                    self.measures.ir_sensor_center = float_of_string(attrs["Value"])
+                elif id == 1:
+                    self.measures.ir_sensor_left = float_of_string(attrs["Value"])
+                elif id == 2:
+                    self.measures.ir_sensor_right = float_of_string(attrs["Value"])
+                elif id == 3:
+                    self.measures.ir_sensor_back = float_of_string(attrs["Value"])
+
+                self.measures.ir_sensor[id] = float_of_string(attrs["Value"])
             else:
                 self.status = -1
+
         elif name == "BeaconSensor":
             id = attrs["Id"]
             # if id<self.measures.beaconReady.len():
-            if 1:
-                self.measures.beaconReady = True
-                # print attrs["Value"]
-                if attrs["Value"] == "NotVisible":
-                    # self.measures.beaconReady[id]=(False,0.0)
-                    self.measures.beacon = (False, 0.0)
-                else:
-                    # self.measures.beaconReady[id]=(True,attrs["Value"])
-                    self.measures.beacon = (
-                        True, float_of_string(attrs["Value"]))
+            # print attrs["Value"]
+            if attrs["Value"] == "NotVisible":
+                # self.measures.beaconReady[id]=(False,0.0)
+                self.measures.beacon = False
             else:
-                self.status = -1
+                # self.measures.beaconReady[id]=(True,attrs["Value"])
+                self.measures.beacon = float_of_string(attrs["Value"])
+
         elif name == "GPS":
             if "X" in attrs.keys():
-                self.measures.gpsReady = True
                 # print attrs["X"]
-                self.measures.x = float_of_string(attrs["X"])
-                self.measures.y = float_of_string(attrs["Y"])
+                self.measures.gps_x = float_of_string(attrs["X"])
+                self.measures.gps_y = float_of_string(attrs["Y"])
                 if "Dir" in attrs.keys():
-                    self.measures.gpsDirReady = True
-                    self.measures.dir = float_of_string(attrs["Dir"])
-                else:
-                    self.measures.gpsDirReady = False
-            else:
-                self.measures.gpsReady = False
+                    self.measures.direction = float_of_string(attrs["Dir"])
+
         elif name == "Leds":
-            self.measures.endLed = (attrs["EndLed"] == "On")
-            self.measures.returningLed = (attrs["ReturningLed"] == "On")
-            self.measures.visitingLed = (attrs["VisitingLed"] == "On")
+            self.measures.end_led = (attrs["EndLed"] == "On")
+            self.measures.returning_led = (attrs["ReturningLed"] == "On")
+            self.measures.visiting_led = (attrs["VisitingLed"] == "On")
         elif name == "Buttons":
             self.measures.start = (attrs["Start"] == "On")
             self.measures.stop = (attrs["Stop"] == "On")
         elif name == "Score":
-            self.measures.scoreReady = ("Score" in attrs.keys())
-            if self.measures.scoreReady:
+            if "Score" in attrs.keys():
                 self.measures.score = int(attrs["Score"])
-            self.measures.arrivalTimeReady = ("ArrivalTime" in attrs.keys())
-            if self.measures.arrivalTimeReady:
-                self.measures.arrivalTime = int(attrs["ArrivalTime"])
-            self.measures.returningTimeReady = (
-                "ReturningTime" in attrs.keys())
-            if self.measures.returningTimeReady:
-                self.measures.returningTime = int(attrs["ReturningTime"])
-            self.measures.collisionsReady = ("Collisions" in attrs.keys())
-            if self.measures.collisionsReady:
+
+            if "ArrivalTime" in attrs.keys():
+                self.measures.arrival_time = int(attrs["ArrivalTime"])
+
+            if "ReturningTime" in attrs.keys():
+                self.measures.returning_time = int(attrs["ReturningTime"])
+
+            if "Collisions" in attrs.keys():
                 self.measures.collisions = int(attrs["Collisions"])
         elif name == "Message":
             self.hearFrom = int(attrs["From"])
@@ -325,11 +335,4 @@ def float_of_string(str):
     """
     Function used to convert a string to a float
     """
-    s = ""
-    for c in str:
-        if c == ",":
-            s += "."
-        else:
-            s += c
-    # print "--" + s + "--"
-    return float(s)
+    return float(str.replace(',', '.'))
--- src/web/static/samples/Python/python_sample.py
+++ src/web/static/samples/Python/python_sample.py
@@ -1,149 +1,115 @@
-#
-# Authors: Luis Seabra Lopes
-# October-November 2013
-#
-
-
-from croblink import *
+from crobotlink import CRobotLink
 import sys
 from math import *
+from socket import *
+import random
 
 
-class MyRob(CRobLink):
-    def __init__(self, rob_name, host, rob_id):
+class MyRobot(CRobotLink):
+    def __init__(self, robot_name, host, robot_id):
+        self.robot_name = robot_name
         self.counter = 0
-        self.prev_ground = 0
-        self.start_pos = 0
-        self.start_saved = False
-        CRobLink.__init__(self, rob_name, rob_id, host)
+        CRobotLink.__init__(self, robot_name, robot_id, host)
+        print "Robot %s Initialized in position %d" % (robot_name, robot_id)
 
     def run(self):
-        if rob.status != 0:
-            print "Connection refused or error"
-            quit()
+        """
+        Main Run loop
+        """
+        if self.status != 0:
+            print "Connection to simulator was refused or returned an error %d" % self.status
+            sys.exit(-1)
 
+        print "Robot Run loop"
         state = 'stop'
-        stopped_state = 'run'
-
-        self.start_saved = False
-        self.prev_ground = 99
 
         while True:
-            self.read_sensors()
+            self.counter += 1
+            print "Counter=%d" % self.counter
 
-            if self.measures.groundReady:
-                if self.measures.ground != self.prev_ground:
-                    self.prev_ground = self.measures.ground
+            # Read Sensors
+            self.request_sensors(['Beacon', 'Ground', 'Compass', 'IRSensor'])
+            self.read_sensors()  # Update sensor measures
 
-            if self.measures.endLed:
-                print self.rob_name + " exiting"
-                quit()
+            # Start button is pressed?
+            if self.measures.start and state != "run":
+                print "Running"
+                state = "run"
 
-            if state == 'stop' and self.measures.start:
-                state = stopped_state
-
-            if state != 'stop' and self.measures.stop:
-                stopped_state = state
+            # Stop button was pressed?
+            if self.measures.stop and state != "stop":
+                print "Stopping"
                 state = 'stop'
 
             if state == 'run':
-                if not self.start_saved:
-                    if self.measures.gpsReady:
-                        self.start_pos = (self.measures.x, self.measures.y)  # posicao inicial
-                        self.start_saved = True
-
-                if not self.measures.visitingLed and \
-                        not self.measures.returningLed and \
-                        self.measures.groundReady and self.measures.ground == 0:
-                    self.set_visiting_led(True)
-
-                if self.measures.visitingLed:  # quando chegar ao queijo...
-                    self.set_visiting_led(False)
-                    self.set_returning_led(True)
-
-                if self.measures.returningLed:
-                    state = 'return'
+                if self.measures.ground == 1:
+                    print "Robot %s reached the target" % (self.robot_name)
+                    left_power = right_power = 0
+                    self.set_visiting_led('On')
+                    self.finish()
 
                 else:
-                    l_pow, r_pow = self.determine_action("run")
-                    self.drive_motors(l_pow, r_pow)
+                    left_power, right_power = self.determine_action("run")
 
-            self.sync_robot()
+                print "Power: %f %f" % (left_power, right_power)
+
+                self.drive_motors(left_power, right_power)
+
+            self.sync_robot()  # Sync with simulator
 
     def determine_action(self, state):
-        collision = False
-        beacon_visible = False
+        """
+        Main function to determine the action to take
+        """
 
-        center_id = 0
-        left_id = 1
-        right_id = 2
-        back_id = 3
-        center = left = right = back = 0
+        left = self.measures.ir_sensor_left
+        right = self.measures.ir_sensor_right
+        center = self.measures.ir_sensor_center
 
-        if self.measures.irSensorReady[left_id]:
-            left = self.measures.irSensor[left_id]
-        if self.measures.irSensorReady[right_id]:
-            right = self.measures.irSensor[right_id]
-        if self.measures.irSensorReady[center_id]:
-            center = self.measures.irSensor[center_id]
-        if self.measures.irSensorReady[back_id]:
-            back = self.measures.irSensor[back_id]
+        beacon = self.measures.beacon
+        collision = self.measures.collision
+        compass = self.measures.compass
 
-        beacon_ready = self.measures.beaconReady
-        if beacon_ready:
-            beacon_visible, beacon_dir = self.measures.beacon
+        try:
+            print "Sensors: Left=%s Center=%s Right=%s Back=%s Collision=%s Beacon=%s" %(str(left), str(center), str(right), str(beacon), str(collision), str(beacon))
+        except:
+            pass
 
-        if self.measures.groundReady:
-            ground = self.measures.ground
-        if self.measures.collisionReady:
-            collision = self.measures.collision
+        left_power = 0  # Left engine power
+        right_power = 0  # Right engine power
 
-        if center > 4.5 or right > 4.5 or left > 4.5 or collision:
-            if self.counter % 400 < 200:
-                l_pow = 0.06
-                r_pow = -0.06
+        #
+        # Add your code here and replace this dummy code!
+        #
 
-            else:
-                l_pow = -0.06
-                r_pow = 0.06
-        elif right > 1.5:
-            l_pow = 0.0
-            r_pow = 0.5
-
-        elif left > 1.5:
-            l_pow = 0.05
-            r_pow = 0.0
+        if collision == 1 or center > 2:
+            print "On Collision Course!"
+            left_power = -1
+            right_power = 1
         else:
-            follow = False
-            if state == 'run' and beacon_ready and beacon_visible:
-                follow = True
-                target_dir = beacon_dir
+            print "Warp Speed!"
+            left_power = 1
+            right_power = 1
 
-            if follow and target_dir > 20.0:
-                l_pow = 0.0
-                r_pow = 0.1
-            elif follow and target_dir < -20.0:
-                l_pow = 0.1
-                r_pow = 0.0
-            else:
-                l_pow = 0.1
-                r_pow = 0.1
+        return left_power*0.15, right_power*0.15
 
-        self.counter += 1
-        return l_pow, r_pow
+#
+# Lets parse the command line
+# Create the Robot and connect to the Simulator
+#
+if __name__ == "__main__":
+    robot_name = "BB"
+    hostname = "localhost:6000"
+    position = 3
 
+    for i in range(0, len(sys.argv)):
+        if sys.argv[i] == "-host" and i != len(sys.argv) - 1:
+            hostname = sys.argv[i + 1]
+        if sys.argv[i] == "-position" and i != len(sys.argv) - 1:
+            position = int(sys.argv[i + 1])
+        if sys.argv[i] == "-robotname" and i != len(sys.argv) - 1:
+            robot_name = sys.argv[i + 1]
 
-rob_name = "BB"
-host = "localhost"
-pos = 3
-
-for i in range(0, len(sys.argv)):
-    if sys.argv[i] == "-host" and i != len(sys.argv) - 1:
-        host = sys.argv[i + 1]
-    if sys.argv[i] == "-pos" and i != len(sys.argv) - 1:
-        pos = int(sys.argv[i + 1])
-    if sys.argv[i] == "-robname" and i != len(sys.argv) - 1:
-        rob_name = sys.argv[i + 1]
-
-rob = MyRob(rob_name, host, pos)
-rob.run()
+    # We are ready to GO!
+    robot = MyRobot(robot_name, hostname, position)
+    robot.run()
